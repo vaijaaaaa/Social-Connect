1e6684206a7ed5464b07e51567cb42529385ce98
vaijaaaaa <vaijuwalker111@gmail.com>
2026-04-08 12:58:45 +0530

    middleware added

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest,NextResponse } from "next/server";
+
+const protectedRoutes = ["/feed","/me","/create"];
+const authRoutes = ["/login","/register"];
+
+export function middleware(request:NextRequest){
+    const {pathname} = request.nextUrl;
+    const accessToken = request.cookies.get("sb-access-toke")?.value;
+
+    const isProtectedRoute = protectedRoutes.some(
+        (route) => pathname === route || pathname.startsWith(`${route}/`),
+    )
+
+    const isAuthRoute = authRoutes.some(
+    (route) => pathname === route || pathname.startsWith(`${route}/`),
+  );
+
+  if(isProtectedRoute && !accessToken){
+    return NextResponse.redirect(new URL("/login",request.url));
+  }
+
+  if(isAuthRoute && accessToken){
+    return NextResponse.redirect(new URL("/feed",request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/feed/:path*", "/me/:path*", "/create/:path*", "/login/:path*", "/register/:path*"],
+};
